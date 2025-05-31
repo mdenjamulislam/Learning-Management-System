60d6edf6ed6e4d668aa8aa1ebe2df0074ba35285
Enjamul Islam <mdenjamulislam527@gmail.com>
2025-05-31 13:25:59 +0600

    Coure List page design

--- src/components/student/SearchBar.jsx
+++ src/components/student/SearchBar.jsx
@@ -14,16 +14,16 @@ const SearchBar = ({data}) => {
     }
 
     return (
-        <form onSubmit={onSearchhandaler} className="max-w-xl w-full md:h-12 h-11 flex items-center bg-white border border-gray-500/20 rounded">
+        <form onSubmit={onSearchhandaler} className="max-w-lg w-full md:h-11 h-10 flex items-center bg-white border border-gray-500/20 rounded">
             <img src={assets.search_icon} alt="Search Icon" className="md:w-auto w-10 px-3" />
             <input
                 onChange={e => setInput(e.target.value)}
                 value={input}
                 type="text"
                 placeholder="Search for courses"
-                className="w-full h-full outline-none text-gray-500/80 px-3"
+                className="w-full h-full outline-none px-3"
             />
-            <button type="submit" className="bg-accent border border-accent rounded text-white md:px-10 px-7 md:py-3 py-2 mx-0 hover:bg-yellow-600">
+            <button type="submit" className="bg-accent border border-accent rounded text-white md:px-9 px-6 py-2 mx-0 hover:bg-yellow-600">
                 Search
             </button>
         </form>
