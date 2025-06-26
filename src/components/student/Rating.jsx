47835d41041750812e9d1569b98550a01a670914
Enjamul Islam <mdenjamulislam527@gmail.com>
2025-06-26 13:35:44 +0600

    Player page design finished and rating component create

--- src/components/student/Rating.jsx
+++ src/components/student/Rating.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
-const Rating = () => {
+const Rating = ({ initialTaring, onRate }) => {
+    const [rating, setRating] = useState(initialTaring || 0);
+
+    const handlerating = (value) => {
+        setRating(value);
+        if (onRate) onRate(value);
+    };
+
+    useEffect(() => {
+        if (initialTaring) {
+            setRating(initialTaring);
+        }
+    }, [initialTaring]);
+
     return (
         <div>
-            Rating
+            {Array.from({ length: 5 }, (_, index) => {
+                const starValue = index + 1;
+                return (
+                    <span key={index} onClick={() => handlerating(starValue)} className={`text-xl sm:text-2xl cursor-pointer transition-colors ${starValue <= rating ? "text-accent" : "text-gray-400 dark:text-gray-300"}`}>
+                        &#9733;
+                    </span>
+                );
+            })}
         </div>
     );
 };
--- src/pages/student/Player.jsx
+++ src/pages/student/Player.jsx
@@ -4,6 +4,8 @@ import { useParams } from "react-router-dom";
 import humanizeDuration from "humanize-duration";
 import { assets } from "../../assets/assets";
 import YouTube from "react-youtube";
+import Footer from "../../components/student/Footer";
+import Rating from "../../components/student/Rating";
 
 const Player = () => {
     const { allCourses, enrolledCourses, calculateChapterTime, calculateNoOfLectures, calculateCourseDuration } = useContext(AppContext);
@@ -26,74 +28,83 @@ const Player = () => {
 
     return (
         <>
-            <div className="p-4 sm:p-10 flex flex-col-reverse md:grid md:grid-cols-2 gap-10 md:px-36">
-                {/* left */}
-                <div className="text-gray-800 dark:text-gray-200">
-                    <h2 className="text-xl font-semibold">Course Structure</h2>
-                    <div className="pt-5">
-                        {course &&
-                            course.courseContent.map((chapter, index) => (
-                                <>
-                                    <div key={index}>
-                                        <div className="">
-                                            <div tabIndex={0} className="collapse collapse-arrow bg-base-100 dark:bg-slate-800 border-base-300 border mb-2">
-                                                <div className="collapse-title font-semibold flex items-center gap-4">
-                                                    {chapter.chapterTitle}
-                                                    <p className="flex items-center gap-1.5 text-sm text-neutral dark:text-white/70">
-                                                        {chapter.chapterContent.length} chapters -
-                                                        <img src={assets.time_clock_icon} alt="" className="w-4" /> {calculateChapterTime(chapter)}
-                                                    </p>
-                                                </div>
-                                                <div className="collapse-content text-sm">
-                                                    <ul className="list-none space-y-3">
-                                                        {chapter.chapterContent.map((lecture, i) => (
-                                                            <li
-                                                                key={i}
-                                                                onClick={() =>
-                                                                    setPlayerData({
-                                                                        ...lecture,
-                                                                        chapter: index + 1,
-                                                                        lecture: i + 1,
-                                                                    })
-                                                                }
-                                                                className="flex items-center gap-2 cursor-pointer"
-                                                            >
-                                                                <img src={false ? assets.blue_tick_icon : assets.play_icon} alt="Play Icon" className="w-4" />
-                                                                <div className="flex flex-row justify-between items-center w-full">
-                                                                    <h4 className="text-sm md:text-base">{lecture.lectureTitle}</h4>
-                                                                    <div className="flex items-center gap-2 text-sm text-neutral dark:text-white/70">
-                                                                        {lecture.lectureUrl && <small className="badge badge-soft badge-success text-xs cursor-pointer">Watch</small>}
-                                                                        <p>{humanizeDuration(lecture.lectureDuration * 60 * 1000, { units: ["h", "m"] })}</p>
+            <div className="container">
+                <div className="flex flex-col-reverse md:grid md:grid-cols-2 gap-10 py-10 md:py-16 lg:py-20">
+                    {/* left */}
+                    <div className="text-gray-800 dark:text-gray-200">
+                        <h2 className="text-xl font-semibold">Course Structure</h2>
+                        <div className="pt-5">
+                            {course &&
+                                course.courseContent.map((chapter, index) => (
+                                    <>
+                                        <div key={index}>
+                                            <div className="">
+                                                <div tabIndex={0} className="collapse collapse-arrow bg-base-100 dark:bg-slate-800 border-base-300 border mb-2">
+                                                    <div className="collapse-title font-semibold flex items-center gap-4">
+                                                        {chapter.chapterTitle}
+                                                        <p className="flex items-center gap-1.5 text-sm text-neutral dark:text-white/70">
+                                                            {chapter.chapterContent.length} chapters -
+                                                            <img src={assets.time_clock_icon} alt="" className="w-4" /> {calculateChapterTime(chapter)}
+                                                        </p>
+                                                    </div>
+                                                    <div className="collapse-content text-sm">
+                                                        <ul className="list-none space-y-3">
+                                                            {chapter.chapterContent.map((lecture, i) => (
+                                                                <li
+                                                                    key={i}
+                                                                    onClick={() =>
+                                                                        setPlayerData({
+                                                                            ...lecture,
+                                                                            chapter: index + 1,
+                                                                            lecture: i + 1,
+                                                                        })
+                                                                    }
+                                                                    className="flex items-center gap-2 cursor-pointer"
+                                                                >
+                                                                    <img src={false ? assets.blue_tick_icon : assets.play_icon} alt="Play Icon" className="w-4" />
+                                                                    <div className="flex flex-row justify-between items-center w-full">
+                                                                        <h4 className="text-sm md:text-base">{lecture.lectureTitle}</h4>
+                                                                        <div className="flex items-center gap-2 text-sm text-neutral dark:text-white/70">
+                                                                            {lecture.lectureUrl && <small className="badge badge-soft badge-success text-xs cursor-pointer">Watch</small>}
+                                                                            <p>{humanizeDuration(lecture.lectureDuration * 60 * 1000, { units: ["h", "m"] })}</p>
+                                                                        </div>
                                                                     </div>
-                                                                </div>
-                                                            </li>
-                                                        ))}
-                                                    </ul>
+                                                                </li>
+                                                            ))}
+                                                        </ul>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
-                                    </div>
-                                </>
-                            ))}
+                                    </>
+                                ))}
+                        </div>
+
+                        {/* Rate this course */}
+                        <div className="flex items-center gap-2 py-3 mt-10">
+                            <h2 className="text-xl font-semibold">Rate this course:</h2>
+                            <Rating initialTaring={0}/>
+                        </div>
+                    </div>
+                    {/* right */}
+                    <div>
+                        {playerData ? (
+                            <div>
+                                <YouTube videoId={playerData.videoId} opts={{ playerVars: { autoplay: 1 } }} iframeClassName="w-full aspect-video" />
+                                <div className="flex items-center justify-between mt-4">
+                                    <p className="text-base font-semibold">
+                                        {playerData.chapter}.{playerData.lecture} {playerData.lectureTitle}
+                                    </p>
+                                    <button className="">{false ? "Complete" : "Mask Complete"}</button>
+                                </div>
+                            </div>
+                        ) : (
+                            <img src={course ? course.courseThumbnail : "Not found"} alt="" className="w-full" />
+                        )}
                     </div>
                 </div>
-                {/* right */}
-                <div>
-                    {playerData ? (
-                        <div>
-                            <YouTube videoId={playerData.videoId} opts={{ playerVars: { autoplay: 1 } }} iframeClassName="w-full aspect-video" />
-                            <div>
-                                <p>
-                                    {playerData.chapter}.{playerData.lecture} {playerData.lectureTitle}
-                                </p>
-                                <button className="">Marke Complete 5h:30m</button>
-                            </div>
-                        </div>
-                    ) : (
-                        <img src={course ? course.courseThumbnail : "Not found"} alt="" className="w-full" />
-                    )}
-                </div>
             </div>
+            <Footer />
         </>
     );
 };
